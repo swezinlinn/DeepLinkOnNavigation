adb2937323e48e7d10f1b6f9cdbc9e81ac835a98
Swe Zin Linn <swezinlinn@Swes-MacBook-Pro.local>
2019-08-08 11:32:01 +0630

    deep link on navigation

--- app/src/main/java/com/example/deeplinkonnavigation/FirstFragment.kt
+++ app/src/main/java/com/example/deeplinkonnavigation/FirstFragment.kt
@@ -1,31 +1,36 @@
 package com.example.deeplinkonnavigation
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        txv_go_to_second.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("http://www.example.com/users")
+            startActivity(openURL)
+        }
+    }
 
 }
